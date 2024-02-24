6a16292f8417ea219c09adfd66002e7054fe08ef
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-02-24 22:59:33 +0530

    tailwind included

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,19 @@ import "./Login.css"
 const Login = () => {
   return (
     <div>
-     I am login
+      <div className="catchy flex mx-10">
+        <h1>Muskuraiye! aap thapar mein hain</h1>
+        <h1>Tumhari maa ki chu</h1>
+      </div>
+      <div className="font-bold">
+        <h1 className="mx-11">Login as:</h1>
+        <div className="font-serif">
+          <button>Student</button>
+          <button>Driver</button>
+          <button>Admin</button>
+        </div>
+      </div>
+     
     </div>
   )
 }
